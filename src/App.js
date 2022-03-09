0d034fdbcb34e78f9eb96b1fd419ceb85ab4b781
Libardo <libardojesusrengifo129@gmail.com>
2022-03-09 15:51:30 -0400

    register page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import withAuth from './withAuth';
 import Home from './Home';
 import Secret from './Secret';
 import Login from './Login';
+import Register from './Register';
 
 class App extends Component {
   render() {
@@ -13,12 +14,14 @@ class App extends Component {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/secret">Secret</Link></li>
           <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
         </ul>
 
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/secret" component={withAuth(Secret)} />
           <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
         </Switch>
       </div>
     );
